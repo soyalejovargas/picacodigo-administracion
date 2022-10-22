7fc8ae0aab31510cc0c8f11683c9489a8466e38d
Jbuendia <jabuendia19@gmail.com>
2022-10-22 16:05:50 -0500

    Cambiando dependencias y agregando metodos faltantes para desplegar.JB

--- src/main/java/com/picacodigo/App.java
+++ src/main/java/com/picacodigo/App.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,12 +16,18 @@ import org.springframework.web.cors.CorsConfiguration;
 
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+	
 	@EnableWebSecurity
 	@Configuration
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
